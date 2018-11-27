a8a7f89107926ac743e8bbbdfe17da92b86b3df3
Peter <pkabai@csumb.edu>
2018-11-27 13:24:34 -0800

    Uses millis to keep track of time, rather than delays

--- randomDriving/randomDriving.ino
+++ randomDriving/randomDriving.ino
@@ -1,24 +1,28 @@
-#define IN1  7  // K1 K2 motor direction
-#define IN2  8  // K1 K2 motor direction
-#define IN3  11 // K3 K4 motor direction
-#define IN4  10 // K3 K4 motor direction
+#define IN1  7  // K1 motor direction
+#define IN2  8  // K1 motor direction
+#define IN3  11 // K3 motor direction
+#define IN4  10 // K3 motor direction
 #define ENA  5  // Motor speed
 #define ENB  6  // Motor speed
+
+unsigned long delayTill = 0;
 
 void backward(int t) {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4,HIGH);
-  delay(t);
+  //delay(t);
+  delayTill = millis() + t;
 }
 
 void forward(int t) {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
-  digitalWrite(IN4,LOW); 
-  delay(t);
+  digitalWrite(IN4,LOW);
+  //delay(t);
+  delayTill = millis() + t;
 }
 
 void stopMotor() {
@@ -26,7 +30,7 @@ void stopMotor() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
-  digitalWrite(IN4,LOW); 
+  digitalWrite(IN4,LOW);
 }
 
 void turnLeft(int t) {
@@ -34,7 +38,8 @@ void turnLeft(int t) {
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  delay(t);
+  //delay(t);
+  delayTill = millis() + t;
 }
 
 void turnRight(int t) {
@@ -42,7 +47,8 @@ void turnRight(int t) {
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  delay(t);
+  //delay(t);
+  delayTill = millis() + t;
 }
 
 void setMotorSpeed(int lspeed,int rspeed) { 
@@ -51,27 +57,39 @@ void setMotorSpeed(int lspeed,int rspeed) {
   analogWrite(ENB,rspeed);   
 }
 
+// keeps track of passed time without using delay
+boolean canContinue() {
+  if (delayTill > millis()) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
 void randomDrive() {
-  int turnDuration = random(250, 1000);   // turn for between 1/4 and 1 seconds
-  int turnDirection = random(0,2);        // random direction to turn in 
-  int driveDuration = random(500, 3000);  // drive durration between 1/2 and 3 seconds
-  int driveDirection = random(0, 2);      // forward or backward direction
-
-  Serial.print(driveDirection);
   
-  // execute turn
-  if (turnDirection == 0) {
-    turnLeft(turnDuration);
-  } else {
-    turnRight(turnDuration);
+  // turn
+  if (canContinue()) {
+    int turnDuration = random(250, 1000);   // turn for between 1/4 and 1 seconds
+    int turnDirection = random(0,2);        // random direction to turn in 
+    if (turnDirection == 0) {
+      turnLeft(turnDuration);
+    } else {
+      turnRight(turnDuration);
+    }
   }
 
-  // excecute drive
-  if (driveDirection == 0) {
-    forward(driveDuration);
-  } else {
-    backward(driveDuration);
-  }
+  //  drive
+  if (canContinue()) {
+    int driveDuration = random(500, 3000);  // drive durration between 1/2 and 3 seconds
+    int driveDirection = random(0, 2);      // forward or backward direction
+    if (driveDirection == 0) {
+      forward(driveDuration);
+    } else {
+      backward(driveDuration);
+    }
+  } 
 }
 
 void setup() {
@@ -90,4 +108,3 @@ void setup() {
 void loop() {
   randomDrive();
 }
-
