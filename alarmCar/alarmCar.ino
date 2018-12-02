224952a4f25d93d39792b400cf6a701d9f8c3235
Peter <pkabai@csumb.edu>
2018-12-01 21:36:18 -0800

    Adds melody to alarm

--- alarmCar/alarmCar.ino
+++ alarmCar/alarmCar.ino
@@ -32,9 +32,188 @@
 #define IR_R 0x00FF5AA5
 #define IR_O 0x00FF38C7
 
+// note definitions for tones
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
 // IRrecv object
 IRrecv IR(irPin);
 decode_results IRresults;
+
+// mario themesong
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+int noteDurations[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12
+};
+
+// starwars
+int melodyStarwars[] = {
+   NOTE_D3, NOTE_D3, NOTE_D3, NOTE_G3, 
+   NOTE_D4, NOTE_C4, NOTE_B3, NOTE_A3, 
+   NOTE_G4, NOTE_D4, NOTE_C4, NOTE_B3, 
+   NOTE_A3, NOTE_G4, NOTE_D4, NOTE_C4, 
+   NOTE_B3, NOTE_C4, NOTE_A3 
+};
+int noteDurationsStarwars[] = {
+  6, 6, 6, 2, 
+  2, 6, 6, 6, 
+  2, 2, 6, 6, 
+  6, 2, 2, 6, 
+  6, 6, 1
+};
+
+// annoying tone
+int melodyAnnoying[] = { 
+  NOTE_D3, NOTE_AS3, NOTE_FS4, NOTE_D5, NOTE_AS5, NOTE_FS6,
+  NOTE_D3, NOTE_AS3, NOTE_FS4, NOTE_D5, NOTE_AS5, NOTE_FS6,
+  NOTE_D3, NOTE_AS3, NOTE_FS4, NOTE_D5, NOTE_AS5, NOTE_FS6,
+  NOTE_D3, NOTE_AS3, NOTE_FS4, NOTE_D5, NOTE_AS5, NOTE_FS6,
+  NOTE_D3, NOTE_AS3, NOTE_FS4, NOTE_D5, NOTE_AS5, NOTE_FS6,
+  NOTE_D3, NOTE_AS3, NOTE_FS4, NOTE_D5, NOTE_AS5, NOTE_FS6,
+  NOTE_D3, NOTE_AS3, NOTE_FS4, NOTE_D5, NOTE_AS5, NOTE_FS6,
+};
+int noteDurationsAnnoying[] = {
+  2, 2, 2, 2, 2, 2,
+  4, 4, 4, 4, 4, 4,
+  8, 8, 8, 8, 8, 8,
+  4, 4, 4, 4, 4, 4,
+  2, 2, 2, 2, 2, 2
+};
 
 // loads the tone library, needed for 
 TonePlayer tone1 (TCCR1A, TCCR1B, OCR1AH, OCR1AL, TCNT1H, TCNT1L);
@@ -66,10 +245,55 @@ unsigned long msInSetCurrentTime = 0; // initial milliseconds in set current tim
 // keeping track of when the alarm should be ringing
 boolean alarmTriggered = false;
 boolean alarmSounding = false;
+unsigned long oneMinPastAlarm = 0;
 
 // keeps track of time for motor movement
 unsigned long delayTill = 0;
 
+// keeps track of time for the tone delay
+unsigned long delayTillNextTone = 0;
+boolean needsPause = false;
+
+// needed for looping through the noted in the melody
+int thisNote = 0;
+int lengthOfMelody = sizeof(noteDurations) / 2;
+
+// plays the next note of the melody, if canPlay is true
+void continueMelody() {
+  int noteDuration = 1000 / noteDurations[thisNote];
+  if (canPlay() && !needsPause) {
+    // the length of the note is 1 second divided by the note type
+    if (melody[thisNote] != 0) {
+      tone1.tone(melody[thisNote]);
+    } else {
+      tone1.noTone();
+    }
+    delayTillNextTone = millis() + noteDuration;
+    needsPause = true;
+    // 30% pasue between notes
+  } else if (canPlay() && needsPause) {
+    tone1.noTone();
+    int pauseBetweenNotes = noteDuration * 0.30;
+    delayTillNextTone = millis() + pauseBetweenNotes;
+    needsPause = false;
+    ++thisNote;
+    if (thisNote == lengthOfMelody) {
+      thisNote = 0;
+    }
+  }
+}
+
+// keeps track of passed time without using delay
+boolean canPlay() {
+  if (delayTillNextTone > millis()) {
+    return false;
+  } else {
+    tone1.noTone();
+    return true;
+  }
+}
+
+// runs every loop to check for IR remote input
 void irTick() {
   if(IR.decode(&IRresults)) {
     if (IRresults.value==IR_1) {
@@ -232,18 +456,10 @@ void keyPressed(String key) {
       currentSet = true;
     }
     else if (key == "O") {
-      Serial.println("Ok button pressed... printing info...");
-      Serial.println("Current time is: " + setCurrent);
-      Serial.println("Alarm time is: " + alarm);
-      beep();
+      alarmTriggered = false;
+      stopMotor();
     }
   }
-}
-
-void alarmTick() {
-    tone1.tone (600);
-    alarmSounding = true;
-    alarmTriggered = false;
 }
 
 // motor movement functions
@@ -353,13 +569,15 @@ void loop() {
   // gets the IR code
   irTick();
 
-  // triggers the alarm
-  if (alarmTriggered & !alarmSounding) {
-    alarmTick();
+  // triggers the alarm melody
+  if (alarmTriggered) {
+    continueMelody();
+  } else { // resets the melody if the alarm is off
+    thisNote = 0;
   }
 
   // random driving if the alarm is sounding
-  if (alarmSounding) {
+  if (alarmTriggered) {
     randomDrive();
   }
   
@@ -388,7 +606,8 @@ void loop() {
     }
 
     // turn the alarm bolean on when needed
-    if (alarmSet & currentSet & alarm == current & !alarmTriggered) {
+    if (alarmSet & currentSet & alarm == current & !alarmTriggered & (millis() > oneMinPastAlarm) ) {
+      oneMinPastAlarm = millis() + 60000;
       alarmTriggered = true;
     }
   }
