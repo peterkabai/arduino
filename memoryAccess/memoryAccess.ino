9504b3b730916ccea88476ff7963feab8348f9a0
Peter <pkabai@csumb.edu>
2018-12-18 20:38:18 -0800

    Adds file to test EEPROM access

---
+++ memoryAccess/memoryAccess.ino
@@ -0,0 +1,37 @@
+#include <EEPROM.h>
+
+struct VarObject {
+  String one;
+  String two;
+};
+
+void setup() {
+  Serial.begin(9600);
+
+  // create a float, store it
+  float f = 123.456f;
+  EEPROM.put(0, f);
+
+  // read back the float, print
+  float valueOutput;
+  EEPROM.get(0, valueOutput);
+  Serial.println(valueOutput);
+
+  // create an object, store it
+  VarObject custom = {
+    "Hello",
+    "World"
+  };
+  EEPROM.put(1, custom);
+
+  // read back the object, print
+  VarObject customVar;
+  EEPROM.get(1, customVar);
+  Serial.println(customVar.one);
+  Serial.println(customVar.two);
+  
+}
+
+void loop() {
+  
+}
