703481d1631f0f6d664f5a2934f3fe328256cf53
Peter <pkabai@csumb.edu>
2019-01-13 18:40:14 -0800

    Adds more serial ports

--- printSerial/readFromUSB.sh
+++ printSerial/readFromUSB.sh
@@ -1,2 +1,3 @@
-# /dev/cu.HC-02-SPP
-cat /dev/cu.usbmodem14201
+# cat /dev/cu.HC-02-SPP
+# cat /dev/cu.usbmodem14201
+cat /dev/cu.wchusbserial1420
--- printSerial/sendToUSB.sh
+++ printSerial/sendToUSB.sh
@@ -1,2 +1,3 @@
-# /dev/cu.HC-02-SPP
-echo $1 > /dev/cu.usbmodem14201
+# echo $1 > /dev/cu.HC-02-SPP
+# echo $1 > /dev/cu.usbmodem14201
+echo $1 > cat /dev/cu.wchusbserial1420
